5bf67603a692c0a651a93b1b284278f49bccacc8
Hari2019041 <hari19041@iiitd.ac.in>
2021-01-13 11:26:26 +0530

    Drawing hangman body, showing letters and word

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pygame.init()
 
 # GLOBAL VARIABLES
 WIDTH = 600
-HEIGHT = 600
+HEIGHT = 650
 MARGIN = 30
 TITLE = "Hangman"
 ICON = pygame.image.load("icon.png")
@@ -18,8 +18,8 @@ BLACK = (0, 0, 0)
 words = []
 with open("dictionary.txt", "r") as file:
     for word in file.readlines():
-        if len(word) >= 4:
-            words.append(word)
+        if len(word) >= 6 and len(word) <= 11:
+            words.append(word[0:-1].upper())
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -34,28 +34,57 @@ def setIcon(ICON):
 
 class Hangman:
     def __init__(self):
-        pass
+        self.stage = 5
+        self.gullotine = [(MARGIN, 350, 150, 5),
+                          (MARGIN+73, 350-300, 5, 300),
+                          (MARGIN+73, 350-300, 120, 5),
+                          (MARGIN+193, 350-300, 5, 50)
+                          ]
+        self.head = [(MARGIN+193, 130), 30]
+        self.body = (MARGIN+193, 160, 5, 80)
+        self.hands = (MARGIN+160, 200, 71, 5)
+        self.rightLeg = ((MARGIN+193, 240),
+                         (MARGIN+193+5, 240),
+                         (MARGIN+160+5, 300),
+                         (MARGIN+160, 300))
+        self.leftLeg = ((MARGIN+193, 240),
+                        (MARGIN+193+5, 240),
+                        (MARGIN+226+5, 300),
+                        (MARGIN+226, 300))
+
+    def drawStage(self):
+        if self.stage == 1:
+            self.drawGullotine()
+        elif self.stage == 2:
+            self.drawHead()
+        elif self.stage == 3:
+            self.drawBody()
+        elif self.stage == 4:
+            self.drawLegs()
+        elif self.stage == 5:
+            self.drawHands()
+        pygame.display.update()
 
     def drawGullotine(self):
-        pass
+        for part in self.gullotine:
+            pygame.draw.rect(screen, BLACK, part)
 
     def drawHead(self):
-        pass
+        self.drawGullotine()
+        pygame.draw.circle(screen, BLACK, self.head[0], self.head[1], 5)
 
     def drawBody(self):
-        pass
+        self.drawHead()
+        pygame.draw.rect(screen, BLACK, self.body)
 
-    def drawRightHand(self):
-        pass
+    def drawLegs(self):
+        self.drawBody()
+        pygame.draw.polygon(screen, BLACK, self.rightLeg)
+        pygame.draw.polygon(screen, BLACK, self.leftLeg)
 
-    def drawLeftHand(self):
-        pass
-
-    def drawRightLeg(self):
-        pass
-
-    def drawLeftLeg(self):
-        pass
+    def drawHands(self):
+        self.drawLegs()
+        pygame.draw.rect(screen, BLACK, self.hands)
 
 
 class Player:
@@ -72,11 +101,14 @@ class Game:
     def __init__(self, player, words):
         self.running = True
         self.font = pygame.font.SysFont("Comic Sans MS", 30)
+        self.wordFont = pygame.font.SysFont("Comic Sans MS", 25)
         self.player = player
         self.words = words
         self.guesses = 0
+        self.maxGuesses = 5
         self.currentWord = ""
-        self.lettersNotGuessed = [self.font.render(
+        self.letters = []
+        self.lettersnotGuessed = [self.font.render(
             chr(i), True, BLACK, WHITE) for i in range(65, 91)]
         self.hangman = Hangman()
 
@@ -87,19 +119,32 @@ class Game:
                     self.running = False
 
     def reset(self):
-        pass
+        self.chooseWord()
 
     def chooseWord(self):
         self.currentWord = random.choice(self.words)
+        for letter in self.currentWord:
+            if letter not in self.letters:
+                self.letters.append(letter)
 
     def showWord(self):
-        pass
+        x = 75 + 45*((10-len(self.currentWord))//2)
+        y = 400
+        for letter in self.currentWord:
+            if letter in self.letters:
+                label = self.wordFont.render(
+                    "__ ", True, BLACK, WHITE)
+            else:
+                label = self.wordFont.render(letter, True, BLACK, WHITE)
+            screen.blit(label, (x, y))
+            x += 45
+        pygame.display.update()
 
     def showLetters(self):
         x = MARGIN
-        y = 420
+        y = 475
         no = 1
-        for letter_label in self.lettersNotGuessed:
+        for letter_label in self.lettersnotGuessed:
             if x == 570:
                 x = MARGIN
             if no > 10:
@@ -110,16 +155,29 @@ class Game:
             no += 1
         pygame.display.update()
 
+    def showBorder(self):
+        pygame.draw.line(screen, BLACK, (0, 10), (WIDTH, 10))
+        pygame.draw.line(screen, BLACK, (0, HEIGHT-30), (WIDTH, HEIGHT-30))
+        pygame.draw.line(screen, BLACK, (0, 10), (WIDTH, 10))
+        pygame.draw.line(screen, BLACK, (0, 10), (WIDTH, 10))
+
     def showScore(self):
-        pass
+        player.showScore()
+
+    def showHangman(self):
+        self.hangman.drawStage()
 
     def mainScreen(self):
         pass
 
     def start(self):
+        self.chooseWord()
         while self.running:
             screen.fill(WHITE)
+            self.showBorder()
             self.showLetters()
+            self.showHangman()
+            self.showWord()
             self.end()
 
 
